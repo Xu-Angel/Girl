577fbcc1f9782699fb3888bd51350ee6c0b738f6
Angel <1049547382@qq.com>
2019-03-25 20:28:21 +0800

    设置admin 打包相对路径，api 请求前缀

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -5,7 +5,7 @@ import { Message } from 'element-ui'
 
 // 创建axios实例
 const service = axios.create({
-  // baseURL: process.env.BASE_API, // api 的 base_url
+  baseURL: process.env.BASE_API, // api 的 base_url
   timeout: 500000 // 请求超时时间
 })
 
