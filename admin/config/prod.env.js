8854062489c3db4f7ccb3b8169dd28e3d73f45ff
Angel <1049547382@qq.com>
2019-03-26 11:55:51 +0800

    线上api 接口域名化

--- admin/config/prod.env.js
+++ admin/config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://123.207.72.208:8088"'
+  // BASE_API: '"http://123.207.72.208:8088"'
+  BASE_API: '"http://gapi.xutianshi.top"'
 }
