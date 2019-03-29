e4b203ec80471c3d207ee2309450bb0a57ca733f
Angel <1049547382@qq.com>
2019-03-29 17:30:51 +0800

    爬取配置页面

--- admin/src/views/spider/config/index.vue
+++ admin/src/views/spider/config/index.vue
@@ -1,26 +1,161 @@
 <template>
-  <div class="app-container">
-    <div>
-      cookie配置
-    </div>
+  <div class="config-container">
+    <el-card>
+      <el-row style="margin-bottom: 20px;">
+        <el-tag type="error" hit="true">爬取详细页配置</el-tag>
+      </el-row>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+      >
+        <el-form-item label="Cookie" prop="cookie">
+          <el-input type="textarea" placeholder="用于模拟登陆状态爬取详细页的隐私数据" v-model="ruleForm.cookie"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="submitForm('ruleForm')">保存配置</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 爬取列表页配置 -->
+    <el-card style="margin-top: 20px;">
+      <el-row style="margin-bottom: 20px;">
+        <el-tag type="error" hit="true">爬取列表页配置</el-tag>
+      </el-row>
+      <el-form
+        :model="ruleForm"
+        :rules="rules1"
+        ref="ruleForm1"
+        label-width="100px"
+      >
+        <el-form-item label="realUid" prop="name">
+          <div style="width:180px;">
+            <el-input placeholder="模拟登陆" v-model="ruleForm.name"></el-input>
+          </div>
+        </el-form-item>
+        <el-form-item label="开始页码" prop="name" style="width:180px">
+            <el-input v-model="ruleForm.pageRange"></el-input>
+        </el-form-item>
+        <el-form-item label="结束页码" prop="name" style="width:180px">
+            <el-input v-model="ruleForm.pageRange"></el-input>
+        </el-form-item>
+        <el-form-item label="区域" prop="region">
+          <el-select style="width: 180px;" v-model="ruleForm.region" placeholder="请选择爬取区域">
+            <el-option v-for="(item, key) in area" :key="key" :label="item" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="年龄" prop="region">
+          <el-select v-model="search.age" clearable style="width: 180px;" placeholder="请选择年龄">
+            <el-option v-for="(item, key) in age" :key="key" :label="item + '岁'" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="学历" prop="region">
+          <el-select v-model="search.education" clearable style="width: 180px;" placeholder="请选择学历">
+            <el-option v-for="(item, key) in education" :key="key" :label="item" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="状态" prop="region">
+          <el-select v-model="search.marriage" clearable style="width: 180px;" placeholder="请选择状态">
+            <el-option v-for="(item, key) in marriage" :key="key" :label="item" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+          <el-form-item label="年龄" prop="region">
+          <el-select v-model="search.age" clearable style="width: 180px;" placeholder="请选择年龄">
+            <el-option v-for="(item, key) in age" :key="key" :label="item + '岁'" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="标签" prop="type">
+          <el-checkbox-group v-model="ruleForm.type">
+            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+            <el-checkbox label="地推活动" name="type"></el-checkbox>
+            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="性别" prop="resource">
+          <el-radio-group v-model="ruleForm.resource">
+            <el-radio label="妹子"></el-radio>
+            <el-radio label="汉子" disabled="true"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="submitForm('ruleForm')">保存配置</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
+import { getSipderConfig } from '@/api/common'
 export default {
   data() {
     return {
-
-    }
+      age: [],
+      height: [],
+      education: [],
+      marriage: [],
+      search: {
+        area: '',
+        age: '',
+        height: '',
+        realUid: '',
+        status: ''
+      },
+      ruleForm: {
+        cookie: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      rules: {
+        cookie: [
+          { required: true, message: '请输入Cookie', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        pageRange: [
+          { required: true, message: '请输入Cookie', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  created() {
+    getSipderConfig().then(rs => {
+      console.log(rs)
+      this.area = rs.data.area
+      this.height = rs.data.height
+      this.age = rs.data.age
+      this.education = rs.data.education
+      this.marriage = rs.data.marriage
+    })
   },
   methods: {
-
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
 }
 </script>
 
 <style scoped>
-.line{
+.line {
   text-align: center;
 }
 </style>
--- admin/src/views/spider/ip/index.vue
+++ admin/src/views/spider/ip/index.vue
@@ -1,33 +1,197 @@
 <template>
   <div class="app-container">
-    <div>
-      <span>:</span>
-      <el-progress :text-inside="true" :stroke-width="18" :percentage="60" status="success"></el-progress>
-      <el-button type="primary">开启<i class="el-icon-upload el-icon--right"></i></el-button>
-      <el-button type="danger" plain disabled>停止</el-button>
+    <div class="search-container">
+      <div class="fr">
+        <el-select v-model="search.education" clearable style="width: 180px;" placeholder="请选择学历">
+          <el-option v-for="(item, key) in education" :key="key" :label="item" :value="item"></el-option>
+        </el-select>
+        <el-select v-model="search.marriage" clearable style="width: 180px;" placeholder="请选择状态">
+          <el-option v-for="(item, key) in marriage" :key="key" :label="item" :value="item"></el-option>
+        </el-select>
+        <el-input v-model="search.realUid" placeholder="请输入realUid" style="width: 180px;"></el-input>
+        <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+      </div>
     </div>
-    <div>
-      cookie配置
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      :row-class-name="tableRowClassName"
+      element-loading-text="Loading"
+      border
+      fit
+      style="width: 100%"
+      @selection-change="selectionChange"
+    >
+    <el-table-column type="selection" width="38"></el-table-column>
+      <el-table-column prop="realUid" align="center" label="realUid" sortable width="95">
+        <template slot-scope="scope">
+          <span @click="toDetailById(scope.row.realUid)">{{ scope.row.realUid }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="nickname" align="center" label="昵称"></el-table-column>
+      <el-table-column prop="randListTag" align="center" label="标签" width="200">
+        <template slot-scope="scope">
+          <div class="span" v-html="scope.row.randListTag"></div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="education" label="学历" align="center"></el-table-column>
+      <el-table-column align="center" prop="status" label="爬取状态" sortable>
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status ? '完成' : '待爬' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="realUid" label="操作">
+        <template slot-scope="scope">
+          <el-button @click="toDetailById(scope.row.realUid,currentPage)">查看</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pagination-container">
+      <el-pagination
+        :total="total"
+        :current-page="currentPage"
+        :page-sizes="[10,20,30,40,50,60,70,80,90,100]"
+        :page-size="pageSize"
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      ></el-pagination>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
+import { getList } from '@/api/table'
+import { getSipderConfig } from '@/api/common'
 
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        未婚: 'success',
+        // draft: 'gray',
+        离异: 'danger',
+        false: 'gray',
+        true: 'success'
+      }
+      return statusMap[status]
     }
   },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      currentPage: 1,
+      total: 10000,
+      pageSize: 10,
+      area: [],
+      search: {
+        area: '',
+        age: '',
+        height: '',
+        realUid: '',
+        status: ''
+      },
+      age: [],
+      height: [],
+      education: [],
+      marriage: [],
+      status: [true, false],
+      uid: 0
+    }
+  },
+  created() {
+    const { uid, pageNum, ...params } = this.$route.query
+    this.currentPage = Number(pageNum) || 1
+    this.search = params || this.search
+    this.uid = uid
+    this.fetchData()
+    getSipderConfig().then(rs => {
+      console.log(rs)
+      this.area = rs.data.area
+      this.height = rs.data.height
+      this.age = rs.data.age
+      this.education = rs.data.education
+      this.marriage = rs.data.marriage
+    })
+  },
   methods: {
+    handleSizeChange() { },
+    selectionChange(rows) {
 
+    },
+    handleCurrentChange(changePage) {
+      // 重新请求本路由页 回到顶部
+      this.$router.push({
+        query: {
+          pageNum: changePage
+        }
+      })
+      this.currentPage = changePage
+      this.fetchData()
+      // 常规请求分页 不回到顶部
+      // this.$route.query.pageNum = changePage
+      // this.currentPage = changePage
+      // this.fetchData()
+    },
+    fetchData() {
+      this.listLoading = true
+      getList({
+        page: this.currentPage,
+        pageSize: this.pageSize,
+        ...this.search
+      }).then(response => {
+        console.log(response)
+        this.list = response.data.items
+        this.total = response.data.total
+        this.listLoading = false
+      })
+    },
+    handleSearch() {
+      console.log(this.search)
+      this.currentPage = 1
+      this.fetchData()
+    },
+    toDetailById(uid, pageNum) {
+      this.$router.push({
+        path: '/girls/detail',
+        query: {
+          uid,
+          pageNum,
+          ...this.search
+        }
+      })
+    },
+    tableRowClassName({ row, rowIndex }) {
+      if (row.realUid === Number(this.uid)) {
+        return 'success-row'
+      }
+      return ''
+    }
   }
 }
 </script>
-
-<style scoped>
-.line{
-  text-align: center;
+<style>
+ .el-table .success-row {
+    background: #f0f9eb;
+  }
+.span span {
+  height: 18px;
+  line-height: 18px;
+  border: 1px solid #f4d6b3;
+  background-color: #fff8f0;
+  color: #ad6623;
+  margin-left: 3px;
+  font-family: 宋体;
+  overflow: hidden;
+  padding: 2px 3px;
 }
-</style>
+.i i {
+  width: 16px;
+  height: 16px;
+  display: inline-block;
+  margin-bottom: -3px;
+  margin-left: 1px;
+}
 
+</style>
