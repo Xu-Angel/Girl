828a8b1b916e118d424ce6216bde9df563ef4ff7
Angel <1049547382@qq.com>
2019-03-29 15:09:04 +0800

    fix: 重定向

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,6 @@ import girl from './girl'
 import spider from './spider'
 import common from './common'
 export default app => {
-  app.get('/', (req, res, next) => {
-  	res.redirect('/')
-  })
   app.use('/admin', admin)
   app.use('/girl', girl)
   app.use('/spider', spider)
