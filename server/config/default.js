17b7ee86464adf0994a4dcff7b1b56ca4810d11e
Angel <1049547382@qq.com>
2019-04-02 22:11:29 +0800

    bv1.1 websocket初探

--- server/config/default.js
+++ server/config/default.js
@@ -2,8 +2,8 @@ module.exports = {
   port: parseInt(process.env.PORT, 10) || 8088,
   // mongoose.connect('mongodb://username:password@host:port/database?options...');
   // url: 'mongodb://127.0.0.1:27017/girldatabase',
-  // url: 'mongodb://root:password123@127.0.0.1:27017/girldatabasev1?authSource=admin',
-  url: 'mongodb://root:password123@123.207.72.208:27017/girldatabasev1-1?authSource=admin',
+  url: 'mongodb://root:password123@127.0.0.1:27017/girldatabasev1-1?authSource=admin',
+  // url: 'mongodb://root:password123@123.207.72.208:27017/girldatabasev1-1?authSource=admin',
   session: {
     name: 'SID',
     secret: 'SID',
