dd0da70fd0db90a66493a84f7d35938ba4a0a96c
Angel <1049547382@qq.com>
2019-04-03 16:06:12 +0800

    v1-1数据库

--- server/config/default.js
+++ server/config/default.js
@@ -2,8 +2,7 @@ module.exports = {
   port: parseInt(process.env.PORT, 10) || 8088,
   // mongoose.connect('mongodb://username:password@host:port/database?options...');
   // url: 'mongodb://127.0.0.1:27017/girldatabase',
-  url: 'mongodb://root:password123@127.0.0.1:27017/girldatabasev1?authSource=admin',
-  // url: 'mongodb://root:password123@123.207.72.208:27017/girldatabasev1-1?authSource=admin',
+  url: 'mongodb://root:password123@127.0.0.1:27017/girldatabasev1-1?authSource=admin',
   session: {   // => *1000 /1000 express-session
     name: 'SID',
     secret: 'SID',
