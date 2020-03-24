c3f1ae372caeafd4a8891b7c548968cd0b616884
Xu-Angel <1049547382@qq.com>
2020-03-24 17:27:08 +0800

    update

--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -3,6 +3,8 @@ import Statistics from '../controller/statistics'
 const router = express.Router()
 
 router.get('/getLine', Statistics.getLine)
+router.get('/getMarriage', Statistics.getMarriage)
+router.get('/getWords', Statistics.getWords)
 // TODO:
 // router.post('/delGirl', Girl.delById);
 
